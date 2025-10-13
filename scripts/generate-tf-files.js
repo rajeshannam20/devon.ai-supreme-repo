4bd2aa9e87fc727e6f78654ca94cc11fa0a6b088
annam.rajesh@softsuave.org <gowthamnataraj.softsuave.com>
2025-10-13 14:43:38 +0530

    Updated infrastructure-ci-cd.yml

--- scripts/generate-tf-files.js
+++ scripts/generate-tf-files.js
@@ -1,3 +1,4 @@
-const fs = require('fs');
-const { terraformManifestYaml } = require('./index');
+import fs from 'fs';
+import { terraformManifestYaml } from '../src/data/manifest/terraform/index.js';
+
 fs.writeFileSync('./src/data/manifest/terraform/main.tf', terraformManifestYaml);
