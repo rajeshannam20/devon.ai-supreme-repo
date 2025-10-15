fedbc2d3160261f777610ca011eb0f4d31c29b4e
annam.rajesh@softsuave.org <gowthamnataraj.softsuave.com>
2025-10-16 01:00:29 +0530

    Updated .cjs files

--- src/data/manifest/terraform/service-mesh/observability.cjs
+++ src/data/manifest/terraform/service-mesh/observability.cjs
@@ -32,7 +32,7 @@ resource "aws_iam_role" "xray_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "xray_role_policy" {
-  count      = var.environment == "prod" && length(data.aws_iam_role.existing_xray_role) == 0 ? 1 : 0
+  count      = var.environment == "prod" ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
   role       = aws_iam_role.xray_role[0].name
 }
